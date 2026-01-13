8ae2519cc6d35a82cc58b90d6945796a586ffbc5
Jarno Vannoppen <vannoppenjarno@gmail.com>
2026-01-13 17:16:40 +0100

    feat: get SQL and LLM prompts

--- src/get/templates.py
+++ src/get/templates.py
@@ -1,4 +1,4 @@
-from config import CONTEXT_PATH, DAILY_PROMPT_TEMPLATE_PATH, RAG_PROMPT_TEMPLATE_PATH
+from config import CONTEXT_PATH, DAILY_PROMPT_PATH, SQL_PROMPT_PATH, LLM_PROMPT_PATH, RAG_PROMPT_PATH
 
 def get_context():
     """
@@ -8,16 +8,28 @@ def get_context():
         context = file.read()
     return context
 
-def get_daily_prompt_template():
+
+def get_daily_prompt():
     """
     Load the daily prompt template from a markdown file.
     """
-    with open(DAILY_PROMPT_TEMPLATE_PATH, "r", encoding="utf-8") as f:
+    with open(DAILY_PROMPT_PATH, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def get_sql_prompt():
+    with open(SQL_PROMPT_PATH, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def get_llm_prompt():
+    with open(LLM_PROMPT_PATH, "r", encoding="utf-8") as f:
         return f.read()
     
+
 def get_rag_prompt():
     """
     Load RAG prompt template from file.
     """
-    with open(RAG_PROMPT_TEMPLATE_PATH, "r", encoding="utf-8") as f:
+    with open(RAG_PROMPT_PATH, "r", encoding="utf-8") as f:
         return f.read()
